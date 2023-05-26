8b1c5cfc42d4e51276c7b1891c55645fe85bf704
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-05-27 00:39:30 +0200

    uvicorn port

--- src/api/start_dev.sh
+++ src/api/start_dev.sh
@@ -8,15 +8,15 @@ python manage.py collectstatic --noinput
 # start "local" celery worker
 C_FORCE_ROOT=false celery -A cidapp.celery_app worker --pool threads --loglevel info &
 
-python manage.py runserver $CAID_HOST:8080
 
 # start django
-#uvicorn CarnivoreIDApp.asgi:application \
-#    --host $CAID_HOST \
-#    --port 8080 \
-#    --log-config logging.yaml \
-#    --log-level info \
-#    --reload
+#python manage.py runserver $CAID_HOST:8080
+uvicorn CarnivoreIDApp.asgi:application \
+    --host $CAID_HOST \
+    --port 8080 \
+    --log-config logging.yaml \
+    --log-level info \
+    --reload
 
 
 #    --host 0.0.0.0 \
