782a4f7d02e7fc4e049ae5efe284116c780e71ca
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-10-13 13:45:59 +0200

    do not delete image when identity is deleted

--- src/api/caidapp/models.py
+++ src/api/caidapp/models.py
@@ -91,6 +91,7 @@ class UploadedArchive(models.Model):
     location_at_upload = models.CharField(max_length=255, blank=True, default="")
     owner = models.ForeignKey(CIDUser, on_delete=models.CASCADE, null=True, blank=True)
     contains_identities = models.BooleanField(default=False)
+    contains_single_taxon = models.BooleanField(default=False)
 
     def __str__(self):
         return str(Path(self.archivefile.name).name)
@@ -135,7 +136,7 @@ class MediaFile(models.Model):
     )
     thumbnail = models.ImageField(blank=True, null=True, max_length=500)
     identity = models.ForeignKey(
-        IndividualIdentity, blank=True, null=True, on_delete=models.CASCADE
+        IndividualIdentity, blank=True, null=True, on_delete=models.SET_DEFAULT
     )
     identity_is_representative = models.BooleanField(default=False)
     updated_by = models.ForeignKey(CIDUser, on_delete=models.CASCADE, null=True, blank=True)
