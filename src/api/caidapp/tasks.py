d995c7bd48f9450d4a6fcb99593e5b91ccb02564
Miroslav Jirik <miroslav.jirik@gmail.com>
2024-04-25 21:55:00 +0200

    fixed string in fgvc set_cuda_device()

--- src/api/caidapp/tasks.py
+++ src/api/caidapp/tasks.py
@@ -589,7 +589,7 @@ def init_identification_on_success(*args, **kwargs):
         message = output["error"]
     else:
         message = ""
-    workgroup.identification_init_message = message
+    workgroup.identification_init_message = message if len(message) < 254 else message[:254]
     now = django.utils.timezone.now()
     workgroup.identification_init_at = now
     workgroup.save()
