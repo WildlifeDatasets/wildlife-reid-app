ddb3ce06720df59f18da9ea6dff24b2a6341cbb1
Miroslav Jirik <miroslav.jirik@gmail.com>
2024-05-24 09:55:47 +0200

    deleted detection worker

--- src/detection_worker/worker_utils/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# general
-RABBITMQ_URL = os.environ["RABBITMQ_URL"]
-REDIS_URL = os.environ["REDIS_URL"]
-
-SHARED_DATA_PATH = os.environ["SHARED_DATA_PATH"]
-RESOURCES_DIR = os.path.join(SHARED_DATA_PATH, "resources")
