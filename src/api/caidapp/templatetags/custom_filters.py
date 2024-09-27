320c92316e1deefcb9713635ed2c8ecd8f857a50
Miroslav Jirik <miroslav.jirik@gmail.com>
2024-09-27 16:56:00 +0200

    floor percents in suggestion

---
+++ src/api/caidapp/templatetags/custom_filters.py
@@ -0,0 +1,21 @@
+from django import template
+import math
+
+register = template.Library()
+
+@register.filter
+def mul(value, arg):
+    """Multiplies the value by the argument."""
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return ''
+
+
+@register.filter
+def floor(value):
+    """Floors the float to the nearest lower integer."""
+    try:
+        return math.floor(float(value))
+    except (ValueError, TypeError):
+        return ''
