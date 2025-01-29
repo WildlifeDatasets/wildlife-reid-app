0b41309a00993aff19e399c051c939764de68666
Miroslav Jirik <miroslav.jirik@gmail.com>
2025-01-29 16:13:55 +0100

    fixed imports of bootstrap js

--- src/api/caidapp/static/caidapp/main.js
+++ src/api/caidapp/static/caidapp/main.js
@@ -1,1 +1,14 @@
 //
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Add click event listener to links with the specific class
+//     document.querySelectorAll('.trigger-loading-modal').forEach(function (link) {
+//         link.addEventListener('click', function (event) {
+//             // Show the loading modal
+//             logger.debug('Showing loading modal');
+//             var loadingModal = new bootstrap.Modal(document.getElementById('loadingModal'));
+//             loadingModal.show();
+//         });
+//     });
+// });
